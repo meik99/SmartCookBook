e5a02091fc86a8127d24ddd3bf6c75cbbce38ad6
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2020-11-26 11:07:57 +0100

    added alternatives to ingredient view

--- src/app/ingredient-list/ingredient-card/ingredient-card.component.ts
+++ src/app/ingredient-list/ingredient-card/ingredient-card.component.ts
@@ -11,6 +11,7 @@ export class IngredientCardComponent implements OnInit {
 
   private _ingredient: any = null;
   alternatives: any[] = [];
+  showAlternatives = false;
 
   constructor(
     private prologService: PrologService
@@ -31,11 +32,28 @@ export class IngredientCardComponent implements OnInit {
   }
 
   toggleShowAlternatives(): void {
-    const name = this.ingredient.links.X.id;
-    console.log(name);
+    this.showAlternatives = !this.showAlternatives;
 
+    const name = this.ingredient.links.X.id;
     this.prologService.answerQuestion(`isAlternativeTo(${name}, X).`)
-      .then(result => console.log(result))
+      .then(result => {
+        this.alternatives = result;
+        this.makeAlternativesDistinct();
+      })
       .catch(err => console.log(err));
+  }
+
+
+  private makeAlternativesDistinct(): void {
+    const distinctAlternatives = [];
+
+    for (const item of this.alternatives) {
+      const existingItem = distinctAlternatives.find(other => other.links.X.id === item.links.X.id);
+      if (!existingItem && item.links.X.id !== this.ingredient.links.X.id) {
+        distinctAlternatives.push(item);
+      }
+    }
+
+    this.alternatives = distinctAlternatives;
   }
 }
